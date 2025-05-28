1659f4f5f4e09e7c99330abead21cba81c35048a
Lucian Chirca <lucian@elnino.tech>
2025-05-28 17:26:05 +0200

    feat: use ArrowWriter to write to buffer

--- src/parquet/generator.rs
+++ src/parquet/generator.rs
@@ -1,4 +1,8 @@
 use crate::parquet::ParquetConfig;
+use std::sync::Arc;
+use arrow::array::{Int64Array, ArrayRef};
+use arrow::record_batch::RecordBatch;
+use parquet::arrow::arrow_writer::ArrowWriter;
 
 pub struct ParquetGenerator {
     config: ParquetConfig,
@@ -10,8 +14,17 @@ impl ParquetGenerator {
     }
 
     pub fn generate(&self) -> Result<(), Box<dyn std::error::Error>> {
-        println!("Generating parquet file: {}", self.config.file_path);
-        // TODO: Implement actual parquet generation logic
+        let col = Arc::new(Int64Array::from_iter_values([1, 2, 3])) as ArrayRef;
+        let to_write = RecordBatch::try_from_iter([("col", col)]).unwrap();
+
+        let mut buffer = Vec::new();
+        let mut writer = ArrowWriter::try_new(&mut buffer, to_write.schema(), None).unwrap();
+
+        writer.write(&to_write).unwrap();
+        writer.close().unwrap();
+
+        println!("{:?}", buffer);
+
         Ok(())
     }
 }
