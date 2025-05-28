b4765f89bd950d36998d88d1dd8cf238cfa0319d
Lucian Chirca <lucian@elnino.tech>
2025-05-28 16:12:45 +0200

    feat: install clap to generate parquet files via the cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    GenerateParquet {
+        file: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    match args.command {
+        Commands::GenerateParquet { file } => {
+            println!("TODO: Generate parquet file at {}", file);
+        }
+    }
 }
