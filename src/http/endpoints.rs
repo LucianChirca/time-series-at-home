43b0c0c2199f8717772827baa03c30595d8a4726
Lucian Chirca <lucian@elnino.tech>
2025-06-03 08:14:20 +0200

    feat(http): update PostData struct to include value and timestamp

--- src/http/endpoints.rs
+++ src/http/endpoints.rs
@@ -6,10 +6,11 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
 pub struct PostData {
-    message: String,
+    value: f64,
+    timestamp: i64,
 }
 
 pub async fn handle_post_data(Json(payload): Json<PostData>) -> (StatusCode, Json<PostData>) {
-    println!("Received POST request with message: {}", payload.message);
+    println!("Received POST request with value: {}", payload.value);
     (StatusCode::OK, Json(payload))
 }
